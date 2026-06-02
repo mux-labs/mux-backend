09c0184e272f2dd455249da0d15d121f70cabcb5
devoclan <jeremiahniffpeter02@gmail.com>
2026-06-02 10:58:21 +0100

    fix(limits): wire PrismaModule so daily spend enforcement works in checkLimits
    - Import PrismaModule in LimitsModule so PrismaService is injected correctly
    - Import PrismaModule in PaymentsModule for PaymentsService DB access
    - checkLimits already aggregates Payment.amount for fromId since start of day

--- src/payments/payments.module.ts
+++ src/payments/payments.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { PaymentsService } from './payments.service';
 import { PaymentsController } from './payments.controller';
 import { LimitsModule } from '../limits/limits.module';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
-  imports: [LimitsModule],
+  imports: [PrismaModule, LimitsModule],
   controllers: [PaymentsController],
   providers: [PaymentsService],
 })
